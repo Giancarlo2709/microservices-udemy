69d658138d9d3efc9bec2e71048ef6027fb58b0f
Giancarlo <giancarlo2709@gmail.com>
2022-05-14 15:20:36 -0500

    add project eureka and eureka client

--- springboot-service-products/src/main/java/com/gyarleque/springboot/app/products/controllers/ProductController.java
+++ springboot-service-products/src/main/java/com/gyarleque/springboot/app/products/controllers/ProductController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
